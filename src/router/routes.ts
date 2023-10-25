68e2d54d191be7d278720ee20403c0dd23a43296
Sancho Cubisino Juan Mateo <j.sancho@etec.local>
2023-10-25 13:02:04 -0300

    Tarea 25/10

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,7 +2,40 @@ import { Router } from "express";
 
 const productRouter = Router();
 
-productRouter.get ("/catalogo,", ( res)=> {
-    res.send("HOLA MUNDO")
+productRouter.get ("/", (_, res) =>{
+    res.send("HOLA MUNDO");
 })
 export default productRouter;
+
+const productosymercancia=[
+    {
+        nombre:"Juan",
+        modelo:"AJD2",
+        precio:1000,
+        paisorigen:"Argentina"
+
+
+    },
+    {
+        nombre:"Ignacio",
+        modelo:"BBC1",
+        precio:500,
+        paisorigen:"Chile"
+
+
+    },
+    {
+        nombre:"Mateo",
+        modelo:"CCI3",
+        precio:3000,
+        paisorigen:"Australia"
+
+
+    }
+];
+
+const enrutador={
+    obtenerproductos:() =>productosymercancia,
+}
+
+console.log(enrutador.obtenerproductos());

99ba9ba542716bc4603615484a46e6a0bc77567c
Sancho Cubisino Juan Mateo <j.sancho@etec.local>
2023-10-25 13:04:57 -0300

    Botinesback

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,35 +7,3 @@ productRouter.get ("/", (_, res) =>{
 })
 export default productRouter;
 
-const productosymercancia=[
-    {
-        nombre:"Juan",
-        modelo:"AJD2",
-        precio:1000,
-        paisorigen:"Argentina"
-
-
-    },
-    {
-        nombre:"Ignacio",
-        modelo:"BBC1",
-        precio:500,
-        paisorigen:"Chile"
-
-
-    },
-    {
-        nombre:"Mateo",
-        modelo:"CCI3",
-        precio:3000,
-        paisorigen:"Australia"
-
-
-    }
-];
-
-const enrutador={
-    obtenerproductos:() =>productosymercancia,
-}
-
-console.log(enrutador.obtenerproductos());

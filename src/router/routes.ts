b25db97175478100240992e22f71dba7d0437e21
Sancho Cubisino Juan Mateo <j.sancho@etec.local>
2023-11-03 12:59:56 -0300

    Botines Reack

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,20 +5,42 @@ const productosymercancia=[
     {nombre:"Ignacio", modelo:"BBC1", precio:200, paisorigen:"Chile"},
     {nombre:"Mateo", modelo:"CCI3", precio:50, paisorigen:"Australia"}
   ];
-
   const router =express.Router()
 
+//punto 1
   router.get("/productosymercancia", (_, res) =>
   {
     res.json(productosymercancia);
   })
   
+//punto 2
   router.get("/productosymercancia/precios/:precios",(req, res)=>{
     const {precios} =req.params;
     const productomayor100=productosymercancia.filter((producto)=>producto.precio > Number(precios) );
     res.json(productomayor100);
   })
 
+//punto 3
+router.put("/productosymercancia/filtrado/:filtrado",(req, res)=>{
+  const {filtrado} =req.params;
+  const productofiltrado=productosymercancia.findIndex((producto)=>producto.modelo=== filtrado );
+  if (productofiltrado !== -1) {
+    const { nombre, precio, origen } = req.body;
+
+    productosymercancia[productofiltrado] = {
+        nombre: nombre || productosymercancia[productofiltrado].nombre,
+        modelo: modelo,
+        precio: precio || productosymercancia[productofiltrado].precio,
+        origen: origen || productosymercancia [productofiltrado].origen,
+    };
+
+    res.send('Producto modificado correctamente');
+  } else {
+    res.status(404).send('Error 404: El producto no existe');
+}
+})
+
+//punto 4
   router.delete("/productosymercancia/modelo/:modelo", (req, res)=>{
       const modelo = req.params.modelo;
       const index = productosymercancia.findIndex((producto)=> producto.modelo==modelo)
@@ -29,13 +51,11 @@ const productosymercancia=[
           res.status(404).json({message:"Producto no encontrado"})
       }
   })
+
+  
   
 
-  router.get("/productosymercancia/filtrado/:filtrado",(req, res)=>{
-    const {filtrado} =req.params;
-    const productomayor100=productosymercancia.filter((producto)=>producto.precio== Number(filtrado) );
-    res.json(productomayor100);
-  })
+  
 
  
   router.get("/productosymercancia/pais/:pais",(req, res)=>{
@@ -43,6 +63,14 @@ const productosymercancia=[
     const productomayor100=productosymercancia.filter((producto)=>producto.paisorigen== String(pais) );
     res.json(productomayor100);
    })
+
+
+   router.post("/create",(req, res) =>{
+    const createproduct = req.body;
+    productosymercancia.push(createproduct); //le hacemos un push al array ya creado que los datos van en el body
+    console.log(productosymercancia);
+    res.status(201).send(productosymercancia);
+  })
 
   export default router;
   
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import productRouter from "../src/router/routes"
 const app = express();
 const port = 8080
 
+app.use(express.json());
 app.use('/', productRouter)
 
 app.listen(port, () => {
